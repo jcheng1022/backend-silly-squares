61e8655001e0e7be9f840ca874f5dbc792eabbf2
Jacky <87586598+jcheng1022@users.noreply.github.com>
2024-09-30 16:03:03 -0400

    game update status

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -2,6 +2,7 @@ import {catchWrapper} from "../utils/errorHandling.js";
 import response from "../utils/response.js";
 import Games from "../models/Games.model.js"
 import {decodeId, encodeId} from "../utils/hashId.js";
+import {GAME_STATE} from "../utils/constants.js";
 
 class GamesControllers {
     @catchWrapper
@@ -22,6 +23,25 @@ class GamesControllers {
         })
     }
 
+    @catchWrapper
+    static async getGameById(req, res) {
+        const {gameId} = req.params
+        const {withPlayers = false } = req.query
+
+        if (!gameId) throw `Missing Game ID`
+        // const query = Games.query().findById(decodeId(gameId))
+
+        // if (withPlayers) query.withGraphFetched('[playerOne,playerTwo]')
+
+        const game = await Games.query().findById(decodeId(gameId))
+
+        return response(res, {
+            code: 200,
+            data:game
+        })
+    }
+
+
 
     @catchWrapper
     static async createGame(req, res) {
@@ -34,7 +54,7 @@ class GamesControllers {
             name,
             password: hashedPassword ?? null,
             ownerId: decodeId(req.user.id),
-            status: "OPEN"
+            status: GAME_STATE.WAITING_JOIN
         })
 
 
@@ -68,8 +88,16 @@ class GamesControllers {
         return response(res, {
             code: 200,
             data: {
-                playerOne: game?.playerOne,
-                playerTwo: game?.playerTwo
+                playerOne: {
+                    id: encodeId(game?.playerOne?.id),
+                    name: game?.playerOne?.name,
+                    isReady: game?.p1Ready
+                },
+                playerTwo: {
+                    id: encodeId(game?.playerTwo?.id),
+                    name: game?.playerTwo?.name,
+                    isReady: game?.p2Ready
+                }
             },
 
         })
@@ -79,6 +107,7 @@ class GamesControllers {
     @catchWrapper
     static async joinGame(req, res) {
        const {gameId} = req.params
+
 
 
         const game = await Games.query().findById(decodeId(gameId))
@@ -106,6 +135,100 @@ class GamesControllers {
         return response(res, {
             code: 200,
             message: "Successfully joined game"
+        })
+    }
+
+
+    @catchWrapper
+    static async toggleReadyStatus(req, res) {
+        const {gameId} = req.params
+
+
+        const game = await Games.query().findById(decodeId(gameId))
+
+        if (!game) throw `Game not found`
+
+        if (!game?.p1 || !game?.p2) throw `Game is missing players`
+
+        if (game?.p1 !== decodeId(req.user.id) && game?.p2 !== decodeId(req.user.id)) throw `You are not in the game`
+
+        if (game?.p1 === decodeId(req.user.id)) {
+            await game.$query().patch({
+                p1Ready: !game?.p1Ready
+            }).where({
+                id: game.id
+            })
+        } else {
+            await game.$query().patch({
+                p2Ready: !game?.p2Ready
+            }).where({
+                id: game.id
+            })
+        }
+
+        return response(res, {
+            code: 200,
+            message: "Successfully joined game"
+        })
+    }
+
+
+    @catchWrapper
+    static async getGameReadyStatuses(req, res) {
+        const {gameId} = req.params
+
+
+        const game = await Games.query().findById(decodeId(gameId))
+
+        if (!game) throw `Game not found`
+
+        let playersAreReady = false
+
+        if (!!game?.p1Ready && !!game?.p2Ready) {
+            playersAreReady = true
+        }
+
+        return response(res, {
+            code: 200,
+            data: {
+                playersAreReady
+            }
+        })
+    }
+
+
+    @catchWrapper
+    static async updateGameStatus(req, res) {
+        const {gameId, type} = req.params
+
+        if (!type) throw `Missing status update type`
+
+        if (!Object.values(GAME_STATE).find(o => o === type)) throw `Invalid status update type`
+
+        if (!gameId) throw `Missing Game ID`
+
+        const game = await Games.query()
+            .findById(decodeId(gameId))
+
+        if (!game)  throw `Game not found`
+
+        const currentStatus = game?.status
+
+        if (currentStatus === type) throw `Game is already ${type}`
+
+        await game.$query().patch({
+            status: type
+        })
+
+
+
+
+
+
+
+        return response(res, {
+            code: 200,
+            message: `Successfully updated game status to ${type}`
         })
     }
 }
--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -7,8 +7,14 @@ const gamesRouter = express.Router();
 gamesRouter.get('/lobbies', authGate, GamesControllers.getAllActiveGames)
 gamesRouter.post('/', authGate, GamesControllers.createGame)
 
+gamesRouter.get('/:gameId', authGate, GamesControllers.getGameById)
+
 gamesRouter.get('/:gameId/players', authGate, GamesControllers.getPlayers)
 gamesRouter.patch('/:gameId/join', authGate, GamesControllers.joinGame)
+gamesRouter.patch('/:gameId/status/:type', authGate, GamesControllers.updateGameStatus)
+
+gamesRouter.patch("/:gameId/ready", authGate, GamesControllers.toggleReadyStatus)
+gamesRouter.get("/:gameId/ready-status", authGate, GamesControllers.getGameReadyStatuses)
 
 
 
